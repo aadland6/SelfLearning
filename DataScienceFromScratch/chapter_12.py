b8c2f1ea5b18d3a6b6df819aa884f6503e426116
Matt Aadland <aadland6@users.noreply.github.com>
2017-07-15 17:21:01 -0400

    finished knn by decreasing k each majority vote

--- DataScienceFromScratch/chapter_12.py
+++ DataScienceFromScratch/chapter_12.py
@@ -9,7 +9,6 @@ from collections import Counter
 
 def euclidean_distance(p, q):
     """Calculates Euclidean distance between p and q in arbitrary dimensions"""
-    print(p, q)
     pq_dims = zip(p, q)
     squared_values = [(x[0] - x[1])**2 for x in pq_dims]
     return math.sqrt(sum(squared_values))
@@ -18,7 +17,8 @@ def majority_vote(labels):
     """Gets the majority vote from a labeled training set"""
     vote_counts = Counter(labels)
     most_common, most_common_count = vote_counts.most_common(1)[0]
-    num_most_common = len(most_common)
+    num_most_common = len([count for count in vote_counts.values() 
+                          if count == most_common_count])
     if num_most_common == 1:
         return most_common
     else:
@@ -26,15 +26,19 @@ def majority_vote(labels):
 
 def toy_knn(k, labeled_points, new_point):
     """Toy KNN Classifier for a set of labeled points and a new one
-       Inputs: labeled_points list of dictionaries in {"label":values} format
+       Inputs: labeled_points list of dictionaries 
     """
     labeled_distances = []
     for point in labeled_points:
         distance_from = euclidean_distance(list(point.values())[0], new_point)
         label = list(point.keys())[0]
-        out_dict = {label:distance_from}
+        out_dict = {"label": label,
+                    "distances": distance_from}
         labeled_distances.append(out_dict)
-    return labeled_distances
+    sorted_distances =  sorted(labeled_distances, key=lambda k: k["label"])
+    sorted_labels = [x["label"] for x in sorted_distances]
+    k_nearest = sorted_labels[:k]
+    return majority_vote(k_nearest)
 
 p = [1, 2, 3, 4]
 q = [2, 3, 4, 5]
@@ -42,8 +46,9 @@ print(euclidean_distance(p, q))
 
 labels = ["apple", "peach", "peach", "cherry"]
 labeled_points = [{"apple":[1,1]}, {"peach":[3,1]}, {"apple":[4,1]}, 
-                   {"cherry":[4,2]},
+                   {"cherry":[4,2]},{"cherry":[4,2]},{"cherry":[4,2]},
+                   {"cherry":[4,2]}
                   ]
 distances = [euclidean_distance(list(x.values())[0], [2,2]) for x in labeled_points]
 new_point = [2,2]
-toy_knn(1, labeled_points, new_point)
+toy_knn(4, labeled_points, new_point)

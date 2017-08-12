83d7dea5b59d0de6a6e2339a7459a26b310f2525
Matt Aadland <aadland6@users.noreply.github.com>
2017-08-12 15:24:28 -0400

    updates to make the linter happy

--- Kaggle/Cooking/cooking_cleaning.py
+++ Kaggle/Cooking/cooking_cleaning.py
@@ -5,15 +5,14 @@ Created on Sat Aug 12 12:28:39 2017
 @author: maadland
 """
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-
-stopwords = ["yellow", "purple", "green", "black", 
-             "purple", "white", "red", "chopped", 
+LEMMATIZER = WordNetLemmatizer()
+STOPWORDS = ["yellow", "purple", "green", "black",
+             "purple", "white", "red", "chopped",
              "diced", "grated", "crushed", "small", "medium",
-             "large", "low-fat", "fresh", "plain", 
-             "all-purpose", "extra-virgin", "nonfat", "dry","reduced-fat", 
+             "large", "low-fat", "fresh", "plain",
+             "all-purpose", "extra-virgin", "nonfat", "dry", "reduced-fat",
              "minced", "plain", "crushed", "chopped", "whole", "brown",
-             "boiling", "shredded", "grated", "pitted", "sliced", "heavy", 
+             "boiling", "shredded", "grated", "pitted", "sliced", "heavy",
              "freshly", "yukon", "gold", "chunky", "low", "sodium", "dark",
              "part-skim", "frozen"]
 
@@ -26,28 +25,29 @@ def remove_stopwords(ingredient, stopwords):
     return new_ingredient
 
 def lemmatize_word(ingredient, lemmatizer):
+    """ Lematizes and ingredient 
+    """
     ingredients = ingredient.split(" ")
     lemmas = [lemmatizer.lemmatize(x) for x in ingredients]
     return " ".join(lemmas)
 
-def shorten_ingredient(ingredient, lemmatizer):
-    """ lemmatizes an ingredient and removes spurious descriptions
+def shorten_ingredient(ingredient):
+    """ Shortens an ingredient to the core term
     """
     terms = set(["chicken", "flour", "milk", "rice", "pork", 
-                 "beef", "lettuce", "beans", "shrimp", "salt"])
+                 "beef", "lettuce", "beans", "shrimp", "salt",
+                 "cream"])
     # get just the overlapping terms in the set
     intersection = set(ingredient.split(" ")).intersection(terms)
     if intersection:
-        new_ingredient = str(list(intersection)[0]) # casts the set to a list for indexing
+        return str(list(intersection)[0]) # casts the set to a list for indexing
     else:
-        new_ingredient = ingredient
-    return new_ingredient
-
+        return ingredient
 
 def remove_colors(ingredient):
     """ removes colors from an ingredient
     """
-    colors = ["yellow", "purple", "green", "black", 
+    colors = ["yellow", "purple", "green", "black",
               "purple", "white", "red"]
     no_colors = [gram for gram in ingredient.split(" ") if gram not in colors]
     colorless_string = " ".join(no_colors)
@@ -56,9 +56,9 @@ def remove_colors(ingredient):
 if __name__ == "__main__":
     row_1 = ["yellow onion", "gram masala", "black pepper"]
     row_2 = ["non-fat 2% milk", "chicken breasts", "all-purpose flour",
-         "olive oil"]
+             "olive oil"]
     row_3 = ["cherry tomatoes", "red potatoes", "pitted olives"]
-    row_1_new = [remove_stopwords(x, stopwords) for x in row_1]
+    row_1_new = [remove_stopwords(x, STOPWORDS) for x in row_1]
     row_2_new = [shorten_ingredient(x) for x in row_2]
-    row_3_new = [lemmatize_word(x, lemmatizer) for x in row_3]
+    row_3_new = [lemmatize_word(x, LEMMATIZER) for x in row_3]
     

619e468c3a0dc774c7edd73e06532bcae81e9e6e
Matt Aadland <aadland6@users.noreply.github.com>
2017-08-12 13:04:20 -0400

    added function to remove stopwords

--- Kaggle/Cooking/cooking_cleaning.py
+++ Kaggle/Cooking/cooking_cleaning.py
@@ -4,8 +4,20 @@ Created on Sat Aug 12 12:28:39 2017
 
 @author: maadland
 """
+stopwords = ["yellow", "purple", "green", "black", 
+             "purple", "white", "red", "chopped", 
+             "diced", "grated", "crushed", "small", "medium",
+             "large", "low-fat", "fresh", "plain", 
+             "all-purpose", "extra-virgin", "nonfat", "dry","reduced-fat", "minced", "plain", "crushed", "chopped", "whole", "brown",
+             "boiling", "shredded", "grated", "pitted", "sliced"]
 
-row_1 = ["yellow onion", "gram masala", "black pepper"]
+def remove_stopwords(ingredient, stopwords):
+    """ removes any words in the stopwords list
+    """
+    no_stops = [gram for gram in ingredient.split(" ") if gram not in stopwords]
+    new_ingredient = " ".join(no_stops)
+    return new_ingredient
+
 def remove_colors(ingredient):
     """ removes colors from an ingredient
     """
@@ -15,4 +27,22 @@ def remove_colors(ingredient):
     colorless_string = " ".join(no_colors)
     return colorless_string
 
-row_1_new = [remove_colors(x) for x in row_1]
+def shorten_ingredient(ingredient):
+    """ shortens an ingredient to the core term
+    """
+    terms = set(["chicken", "flour", "milk", "rice"])
+    # get just the overlapping terms in the set
+    intersection = set(ingredient.split(" ")).intersection(terms)
+    if intersection:
+        return str(list(intersection)[0]) # casts the set to a list for indexing
+    else:
+        return ingredient
+
+
+if __name__ == "__main__":
+    row_1 = ["yellow onion", "gram masala", "black pepper"]
+    row_2 = ["non-fat 2% milk", "chicken breasts", "all-purpose flour",
+         "olive oil"]
+    row_1_new = [remove_stopwords(x, stopwords) for x in row_1]
+    row_2_new = [shorten_ingredient(x) for x in row_2]
+    
